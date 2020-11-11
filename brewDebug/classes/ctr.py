20a6f0f1007ce6cc8031473a49fbf9f0b333af49
TurtleP <jpostelnek@outlook.com>
2020-11-11 00:06:50 -0500

    3ds:  no exception parser not installed error

--- brewDebug/classes/ctr.py
+++ brewDebug/classes/ctr.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 from pathlib import Path
 
@@ -13,12 +14,17 @@ class CTR(Console):
         self.command   = Path(os.environ["DEVKITARM"]) / "bin/arm-none-eabi-addr2line"
         self.arguments = "-aipfCe arm -e"
 
+        self.log_parser = "luma3ds_exception_dump_parser"
+
         if args.log:
             self.parse_log(args.log)
         else:
             self.addr2line(args.pc, args.lr)
 
     def parse_log(self, log):
+        if shutil.which(self.log_parser) is None:
+            return print("error: luma3ds exception parser not installed.")
+
         try:
             subprocess.run(f"luma3ds_exception_dump_parser {log}", shell=True)
         except Exception as exception:
